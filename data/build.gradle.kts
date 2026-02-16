970ac0ccaa29ea275c43486f0e9d58a9c79c1ed6
Stepan <esastep20@gmail.com>
2026-02-16 12:56:23 +0300

    feat: add some module structure

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,22 +1,18 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
 }
 
-
 android {
-    namespace = "com.example.coursesapp"
+    namespace = "com.example.data"
     compileSdk {
         version = release(36)
     }
 
     defaultConfig {
-        applicationId = "com.example.coursesapp"
         minSdk = 24
-        targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -36,11 +32,6 @@ android {
 
 dependencies {
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,5 @@ dependencyResolutionManagement {
 
 rootProject.name = "CoursesApp"
 include(":app")
- 
+include(":data")
+include(":domain")
